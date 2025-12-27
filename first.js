a65dc71d283c5b710332d90c1fc0d0702344ab4a
Aashutosh Karna <karnaaashutosh009@gmail.com>
2025-12-28 02:11:48 +0545

    first though principle

---
+++ first.js
@@ -0,0 +1,16 @@
+const header1 = document.createElement('h1');
+header1.innerText = "Hello World.";
+header1.style.backgroundColor = "blue";
+header1.style.fontSize = "30px";
+header1.style.color = "white";
+
+const header2 = document.createElement('h2');
+header2.innerText = "My name is Aashutosh Karna.";
+header2.style.backgroundColor = "black";
+header2.style.fontSize = "30px";
+header2.style.color = "white";
+
+const root = document.getElementById('root');
+root.append(header1);
+root.append(header2);  
+
