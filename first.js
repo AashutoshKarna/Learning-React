933ed6d163b2a6327919b9d457a354121a44d310
Aashutosh Karna <karnaaashutosh009@gmail.com>
2025-12-28 02:22:10 +0545

    created a render method in ReactDOM object

--- first.js
+++ first.js
@@ -13,6 +13,15 @@ const React = {
 const header1 = React.createElement('h1',{backgroundColor:"blue", color:"white"},"Hello, I am Aashutosh Karna.");
 const header2 = React.createElement('h2',{backgroundColor:"blue", color:"white"},"Hello, I am Karna.");
 
+const ReactDOM = {
+    render: function(element,root){
+        root.append(element);
+    }
+}
+
+ReactDOM.render(header1,document.getElementById('root'));
+ReactDOM.render(header2,document.getElementById('root'));
+
 // const header1 = document.createElement('h1');
 // header1.innerText = "Hello World.";
 // header1.style.backgroundColor = "blue";
@@ -25,7 +34,7 @@ const header2 = React.createElement('h2',{backgroundColor:"blue", color:"white"}
 // header2.style.fontSize = "30px";
 // header2.style.color = "white";
 
-const root = document.getElementById('root');
-root.append(header1);
-root.append(header2);  
+// const root = document.getElementById('root');
+// root.append(header1);
+// root.append(header2);  
 
